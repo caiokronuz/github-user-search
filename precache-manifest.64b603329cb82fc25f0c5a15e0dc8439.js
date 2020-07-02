c559f7a2f68dca269c7446a7349af1bb89d10fec
caiokronuz <caiogabriel135@gmail.com>
2020-07-02 11:55:29 -0300

    Updates

---
+++ precache-manifest.64b603329cb82fc25f0c5a15e0dc8439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6ca5b2196d2f6cdf4d24a752d54d69",
+    "url": "/github-user-search/index.html"
+  },
+  {
+    "revision": "edbd5ce5c22438ebacc0",
+    "url": "/github-user-search/static/css/main.db63236e.chunk.css"
+  },
+  {
+    "revision": "11c3346f073fb52582a2",
+    "url": "/github-user-search/static/js/2.978c71c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-user-search/static/js/2.978c71c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbd5ce5c22438ebacc0",
+    "url": "/github-user-search/static/js/main.916fe65e.chunk.js"
+  },
+  {
+    "revision": "21cf0eaee0fd1ddffbb0",
+    "url": "/github-user-search/static/js/runtime-main.59304db2.js"
+  }
+]);
